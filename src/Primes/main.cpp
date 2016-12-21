87f09f6ef95f090c5a1765b87e4ec7ffbd4edd17
Benjamin Kane <bbk1524@gmail.com>
2016-12-21 15:56:47 -0600

    Primes work. TODO, fix compile options

--- src/Primes/main.cpp
+++ src/Primes/main.cpp
@@ -1,6 +1,9 @@
-#include <iostream>
+#include <cstdlib>
+#include <cstdio>
 #include <cstring>
-#include <memory>
+#include <iostream>
+#include <vector>
+#include <string>
 
 size_t get_number()
 {
@@ -12,28 +15,72 @@ size_t get_number()
     return number;
 }
 
-int main()
+void print_numbers(const std::vector<bool>& numbers)
 {
-    // Sieve of Erosthenes approach
-    // int number = get_number();
-    size_t number = 5;
+    // for (size_t i = 0; i < numbers.size(); i++)
+    // {
+    //     std::cout << "(" << i << " " << numbers[i] << ")" << " ";
+    // }
+    // std::cout << "\n";
 
-    std::cout << number << std::flush << std::endl;
-
-    // std::unique_ptr<bool[]> primes_array(new bool[number]{0});
-    std::unique_ptr<int[]> primes_array(new int[number]);
-
-    // everything is prime at first
-    std::memset(primes_array.get(), 1, number * sizeof(int));
-
-    // start with 2
-    // go through the rest of them and init all multiple of 2 to 0
-    // start wtih two again and find the next known prime
-    // loop through the others again and do the same thing
-    primes_array[2] = 1;
-    for(size_t i = 0; i < number; ++i)
+    for (size_t i = 0; i < numbers.size(); i++)
     {
+        if (numbers[i]) { std::cout << i << " "; }
+    }
+    std::cout << "\n";
+}
 
+void print_to_file(const std::vector<bool>& numbers)
+{
+    FILE* fp = fopen("out_file.txt", "w");
+
+    for (size_t i = 0; i < numbers.size(); i++)
+    {
+        if (numbers[i]) { fprintf(fp, "%lu\n", static_cast<unsigned long>(i)); }
     }
 
+    fclose(fp);
+}
+
+
+int main(int argc, char** argv)
+{
+    // ditch the warning
+    (void)argc;
+
+    char * end = nullptr;
+    // max number is 18446744073709551615
+    size_t number = static_cast<size_t>(strtoul(argv[1], &end, 10));
+    size_t size = number + 1;
+
+    if (number < 2)
+    {
+        std::cout << "No primes less than 2" << std::endl;
+        return 0;
+    }
+
+    std::vector<bool> numbers(size, true);
+
+    if (number > numbers.max_size())
+    {
+        std::cout << "std::vector only has a max size of " << number << std::endl;
+        return 1;
+    }
+
+    numbers[0] = false;
+    numbers[1] = false;
+
+    size_t prime = 2;
+    while (prime < size)
+    {
+        for(size_t i = 2 * prime; i < size; i = i + prime)
+        {
+            numbers[i] = false;
+        }
+        ++prime;
+        // TODO: if I increment by 2 here, I skip numbers like 25. Why?
+        while(!numbers[prime] && prime < size) { prime += 1; }
+    }
+    // print_numbers(numbers);
+    print_to_file(numbers);
 }
